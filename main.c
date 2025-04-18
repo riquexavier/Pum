6a7dec3d7a936f04f2e88ee38fc1d6f5a1f12ddc
riquexavier <pedrooohhenriquex@hotmail.com>
2025-04-17 21:29:40 -0300

    feito repet for

--- main.c
+++ main.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n, i, add=0;
+
+    scanf("%i", &n);
+
+    for(i = 0; n > i; i++)
+    {
+        printf("%i %i %i PUM\n", add+1, add+2, add+3);
+        add = add + 4;
+    }
+    system("pause");
     return 0;
 }
